303ae6dbd6290550c3e0aacff147751c02cc0a77
sbelikov98 <sbelikov@isgneuro.com>
2022-04-26 03:36:04 +0300

    Added PotentialTransformers primitives

---
+++ DTCD-ExtensionElectricalSchemePrimitives/src/primitives/PotentialTransformer/list.js
@@ -0,0 +1,4 @@
+import TwoWinding from './TwoWinding/Transformer';
+import ThreeWinding from './ThreeWinding/Transformer';
+
+export default [TwoWinding, ThreeWinding];
--- DTCD-ExtensionElectricalSchemePrimitives/src/primitives/primitivesList.js
+++ DTCD-ExtensionElectricalSchemePrimitives/src/primitives/primitivesList.js
@@ -1,5 +1,6 @@
 import Breakers from './Breaker/list';
 import PowerTransformers from './PowerTransformer/list';
+import PotentialTransformers from './PotentialTransformer/list';
 import BusbarSection_6kv from './BusbarSection/BusbarSection_6kv';
 import BusbarSection_35kv from './BusbarSection/BusbarSection_35kv';
 import ConnectivityNode from './ConnectivityNode/ConnectivityNode';
@@ -7,6 +8,7 @@ import ConnectivityNode from './ConnectivityNode/ConnectivityNode';
 export default [
   ...Breakers,
   ...PowerTransformers,
+  ...PotentialTransformers,
   BusbarSection_6kv,
   BusbarSection_35kv,
   ConnectivityNode,
