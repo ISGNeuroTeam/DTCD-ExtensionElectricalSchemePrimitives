d461556518955119b3e69267a9fb06f8ec381e23
sbelikov98 <sbelikov@isgneuro.com>
2022-04-26 03:33:52 +0300

    Added ConnectivityNode primitive

--- DTCD-ExtensionElectricalSchemePrimitives/src/primitives/primitivesList.js
+++ DTCD-ExtensionElectricalSchemePrimitives/src/primitives/primitivesList.js
@@ -2,10 +2,12 @@ import Breakers from './Breaker/list';
 import PowerTransformers from './PowerTransformer/list';
 import BusbarSection_6kv from './BusbarSection/BusbarSection_6kv';
 import BusbarSection_35kv from './BusbarSection/BusbarSection_35kv';
+import ConnectivityNode from './ConnectivityNode/ConnectivityNode';
 
 export default [
   ...Breakers,
   ...PowerTransformers,
   BusbarSection_6kv,
   BusbarSection_35kv,
+  ConnectivityNode,
 ];
